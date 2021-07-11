7b5a5800b3f134feabc314c1e74e4147d1fe4281
Sameh Ramadan <sameh.r.sayed@gmail.com>
2021-07-11 23:17:24 +0200

    update config

--- kube/cluster.sh
+++ kube/cluster.sh
@@ -8,8 +8,18 @@ aws ec2 create-key-pair --region us-east-2 --key-name udacity
 
 # Step 2:
 # create cluster
-eksctl create cluster --name shorty --region us-east-2 \
-    --with-oidc --ssh-access --ssh-public-key udacity --managed
+eksctl create cluster \
+--name shorty \
+--version 1.19 \
+--region us-east-2 \
+--nodegroup-name shorty-workers \
+--node-type t2.micro \
+--nodes 2 \
+--nodes-min 1 \
+--nodes-max 3 \
+--node-ami auto \
+--zones us-east-2a \
+--zones us-east-2b 
 
 # Step 3:
 # create deployment using deployment.yml
